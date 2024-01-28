e880cfeb726e265b7ca09e18eab2442d3e4b28e4
Leidy Mitzary Hernandez Ortiz <2230142@upv.edu.mx>
2024-01-28 10:00:50 -0600

    Avance

---
+++ ProgramarioUno/src/main/java/edu/upvictoria/fpoo/ProblemaVeinticuatro.java
@@ -0,0 +1,38 @@
+package edu.upvictoria.fpoo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class ProblemaVeinticuatro {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        double Sueldo = 0;
+        double Ahorro = 0.15;
+        int Mes = 4;
+        int Anio = 12;
+        double Semana;
+        double Mensual;
+        double Anual;
+
+        try {
+
+            System.out.println("--------------------AHORRO-------------------------");
+            System.out.println("Ingrese el sueldo:");
+            Sueldo = Double.parseDouble(bf.readLine());
+
+            Semana = Sueldo * Ahorro;
+
+            Mensual = Semana * Mes;
+
+            Anual = Mensual * Anio;
+
+            System.out.println("El ahorro anual es: $" + Anual);
+
+        } catch (IOException | NumberFormatException e) {
+            System.out.println();
+            return;
+        }
+    }
+}
--- ProgramarioUno/src/main/java/edu/upvictoria/fpoo/ProblemaVeintitres.java
+++ ProgramarioUno/src/main/java/edu/upvictoria/fpoo/ProblemaVeintitres.java
@@ -1,4 +1,37 @@
 package edu.upvictoria.fpoo;
 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class ProblemaVeintitres {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        double precioOriginal = 0;
+        double descuento = 0.20;
+        double iva = 0.15;
+        double precioDescuento;
+        double precioFinal;
+
+        try {
+
+            System.out.println("----------------PRECIO DE ARTICULO-------------------");
+
+            System.out.println("Ingrese el precio del articulo:");
+            precioOriginal = Double.parseDouble(bf.readLine());
+
+            precioDescuento = precioOriginal * (1 - descuento);
+
+            precioFinal = precioDescuento * (1 + iva);
+
+            System.out.println("Precio con descuento: $" + precioDescuento);
+            System.out.println("Precio final con IVA: $" + precioFinal);
+
+        } catch (IOException | NumberFormatException e) {
+            System.out.println();
+            return;
+        }
+    }
 }
